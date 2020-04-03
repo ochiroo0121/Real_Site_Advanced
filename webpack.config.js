96ea11019d8e09160ef159293f221327c6e2020a
ochiroo.beatz <59750754+ochiroo0121@users.noreply.github.com>
2020-04-03 14:14:01 +0200

    about huudas bolon logo hiilee

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,42 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundled.js'
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundled.js"
+  },
+  devServer: {
+    before: function(app, server) {
+      server._watch("./src/**/*.html");
     },
-    devServer: {
-        before: function (app, server) {
-            server._watch("./src/**/*.html")
-        },
-        contentBase: path.join(__dirname, 'src'),
-        compress: true,
-        port: 3000
-    },
+    contentBase: path.join(__dirname, "src"),
+    hot: true,
+    port: 3000
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    "style-loader",
-                    "css-loader",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
 
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            plugins: [
-                                require("postcss-import"),
-                                require("postcss-simple-vars"),
-                                require("postcss-nested"),
-                                require("autoprefixer"),
-                                require("cssnano"),
-                            ]
-                        }
-                    }
-                ]
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [
+                require("postcss-import"),
+                require("postcss-simple-vars"),
+                require("postcss-nested"),
+                require("autoprefixer"),
+                require("cssnano")
+              ]
             }
+          }
         ]
-    }
-
-}
+      }
+    ]
+  }
+};
