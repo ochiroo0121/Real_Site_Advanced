70f2c570acdef3a593c695804586d9611b8c876e
ochiroo.beatz <59750754+ochiroo0121@users.noreply.github.com>
2020-04-06 01:36:04 +0200

    Video Page-n toggle deer Body overflow hiideg bolgoloo

--- src/scripts/toggle_menu.js
+++ src/scripts/toggle_menu.js
@@ -1,4 +1,5 @@
 const toggle = document.querySelector(".nav_toggle");
+const videoBody = document.querySelector(".videoBody");
 const nav__container = document.querySelector(".nav__container");
 const content = document.querySelector(".content");
 const indicator = document.querySelector(".side-indicator");
@@ -8,6 +9,10 @@ toggle.addEventListener("click", () => {
   nav__container.classList.toggle("change");
   content.classList.toggle("content_a");
   indicator.classList.toggle("indicator_a");
+});
+
+toggle.addEventListener("click", () => {
+  videoBody.classList.toggle("bodyChange");
 });
 
 export default toggle;
