4358d04a2e975de37ff439fa4ede7623d3a19d67
ochiroo.beatz <59750754+ochiroo0121@users.noreply.github.com>
2020-04-05 20:48:19 +0200

    video_page-g video player-g ajildag bolgoloo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "../src/styles/styles.css";
 import slide from "../src/scripts/library/home_page";
 import toggle from "../src/scripts/toggle_menu";
+import toTop from "../src/scripts/toTop";
