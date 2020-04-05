5e71691fa2fa42d4697558ee4f3f9ea0e5966ff0
ochiroo.beatz <59750754+ochiroo0121@users.noreply.github.com>
2020-04-05 20:08:52 +0200

    video_page-g buren zasch duuslaa_@media_ok

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,15 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundled.js"
+    filename: "bundled.js",
   },
   devServer: {
-    before: function(app, server) {
+    before: function (app, server) {
       server._watch("./src/**/*.html");
     },
     contentBase: path.join(__dirname, "src"),
     hot: true,
-    port: 3000
+    port: 3000,
   },
 
   module: {
@@ -28,15 +28,16 @@ module.exports = {
             options: {
               plugins: [
                 require("postcss-import"),
+                require("postcss-mixins"),
                 require("postcss-simple-vars"),
                 require("postcss-nested"),
                 require("autoprefixer"),
-                require("cssnano")
-              ]
-            }
-          }
-        ]
-      }
-    ]
-  }
+                require("cssnano"),
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
