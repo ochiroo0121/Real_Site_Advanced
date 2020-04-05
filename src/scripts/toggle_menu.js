9b07f99a9e0303e34bafd130e8fb7d73d677f25f
ochiroo.beatz <59750754+ochiroo0121@users.noreply.github.com>
2020-04-05 21:36:21 +0200

    Mobile menu-g zasch duuslaa

--- src/scripts/toggle_menu.js
+++ src/scripts/toggle_menu.js
@@ -1,11 +1,11 @@
 const toggle = document.querySelector(".nav_toggle");
-const nav = document.querySelector(".nav_ul");
+const nav__container = document.querySelector(".nav__container");
 const content = document.querySelector(".content");
 const indicator = document.querySelector(".side-indicator");
 
 toggle.addEventListener("click", () => {
   toggle.classList.toggle("active");
-  nav.classList.toggle("change");
+  nav__container.classList.toggle("change");
   content.classList.toggle("content_a");
   indicator.classList.toggle("indicator_a");
 });
